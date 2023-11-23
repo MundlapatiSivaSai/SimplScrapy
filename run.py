0f90fad98324c90551a23b77a926b783d059b227
MundlapatiSivaSai <115808608+MundlapatiSivaSai@users.noreply.github.com>
2023-11-23 15:26:17 +0530

    Update run.py
    Updating

--- run.py
+++ run.py
@@ -1,4 +1,10 @@
 from app import app
+import os
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # Get the PORT environment variable (set by Render), default to 5000 if not set
+    port = int(os.environ.get('PORT', 5000))
+    
+    # Bind to 0.0.0.0 to make the application accessible on all public IPs
+    # Set debug to False for production deployment
+    app.run(host='0.0.0.0', port=port, debug=False)
